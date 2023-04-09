f25d06a611241f23b024b9f92cac7005b43c89ca
Aamin887 <forkhamin@yahoo.co.uk>
2023-04-09 09:53:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+
+function authenticate(req, res, next){
+    let authheader = req.headers.authorization
+
+    if(!authheader){
+        let err = new Error('You are not authenticated!!')
+        res.setHeader('WWW-Authenticate', 'Basic')
+        res.status = 401
+        return next(err)
+    }
+
+    let auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':')
+
+    let user = auth[0]
+    let pass = auth[1]
+
+    if(user == 'amin' && pass == '1234'){
+        next()
+    }else{
+        let err = new Error('You are not authenticated!!')
+        res.setHeader('WWW-Authenticate', 'Basic')
+        res.status = 401
+        return next(err)
+    }
+
+}
+
+
+
+app.use(express.static(`${__dirname}/public`))
+app.use(authenticate)
+
+app.get('/', (req, res)=>{
+    res.send('Hello gain')
+})
+app.listen(4000, function(){
+    console.log('Server active on port 4000')
+})
